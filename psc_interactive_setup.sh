b7fb94c1ac3c5c2b096c044595dbbf1c7af53e0e
Brainana <jerry.max.xu@gmail.com>
2025-08-31 16:01:31 -0400

    PSC setup script

--- psc_interactive_setup.sh
+++ psc_interactive_setup.sh
@@ -1,11 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 
-export PROJECT_DIR="~/OCEANDIR"
+export PROJECT_DIR="/ocean/projects/bio250072p/jxu23"
 
 source $PROJECT_DIR/src/venv/bin/activate
 
 export PRIMES_ENV="psc"
 
-export DATA_DIR="$PROJECT_DIR/testing_data"
+export DATA_DIR="$PROJECT_DIR/training_data"
 
 export MODEL_SAVE_DIR="$PROJECT_DIR/models"
